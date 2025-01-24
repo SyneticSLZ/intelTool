971146f5696126b3a3943b93680e77bc1f335aeb
Synetic1 <syneticslz@gmail.com>
2025-01-24 19:40:18 +0000

    demant metrics

---
+++ DemantData.js
@@ -0,0 +1,24 @@
+const DemantData = {
+    key_financials: {
+        revenue: {
+            total: 11087,
+            organic_growth: 3
+        },
+        profitability: {
+            gross_profit: 8510,
+            gross_margin: 76.8,
+            EBITDA: {
+                before_special_items: 2897,
+                margin: 26.1,
+                reported: 2192
+            }
+        }
+    },
+    operational_metrics: {
+        research_development: {
+            total_spend: 733,
+            as_percent_of_sales: 6.6,
+            capitalized: 102
+        }
+    }
+};
